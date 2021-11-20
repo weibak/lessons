bd19cd924719d999914dddbbcdf7306b4d819a56
Artem Sheibak <sheibakaa@gmail.com>
2021-11-20 15:58:27 +0300

    add classwork3 file

--- lesson-05/classwork-03.py
+++ lesson-05/classwork-03.py
@@ -5,9 +5,24 @@
 """
 
 
-def my_func(func_type, *args):
-        result = 0
-        for x in args:
-            result += x
-        return result
-print(my_func())
+my_list = input("введите список чисел через пробел: ").split()
+print("ввденый список: ", my_list)
+num_list = list(map(int, my_list))
+
+input_func_type = input("Если нужно максимльное значение: 1, если минимальное: 2 ")
+func_type = int(input_func_type)
+
+def my_max_min(func_type, my_list):
+    if func_type == 1:
+        print(max(num_list))
+    elif func_type == 2:
+        print(min(num_list))
+    else:
+        print("error")
+    return
+
+my_max_min(func_type, my_list)
+
+
+#print(my_sum(my_list))
+#print(max(num_list))
