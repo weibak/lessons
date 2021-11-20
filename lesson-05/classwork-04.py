1719d066dd44cad76ddb551d7c66dadaae8ccc43
Artem Sheibak <sheibakaa@gmail.com>
2021-11-20 16:26:49 +0300

    add classwork4 file

--- lesson-05/classwork-04.py
+++ lesson-05/classwork-04.py
@@ -3,3 +3,20 @@
 к которому относится этот месяц. Например, передаем 2, на выходе получаем "Winter".
 
 """
+
+month = int(input("Введите номер месяца: "))
+
+
+def month_to_season(month):
+    if (0 < month <= 2 or month == 12):
+        print("Winter")
+    elif (3 <= month <= 5):
+        print("Spring")
+    elif (6 <= month <= 8):
+        print("Summer")
+    elif (9 <= month <= 11):
+        print("Autumn")
+    else:
+        print("Что-то пошло не так!")
+
+month_to_season(month)
