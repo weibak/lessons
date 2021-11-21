35b05b30a28f0e2dd8aae57adfa8422d92eaac78
Artem Sheibak <sheibakaa@gmail.com>
2021-11-21 15:45:10 +0300

    add solution-03 file, and load_license_file.txt

--- lesson-05/solution-03.py
+++ lesson-05/solution-03.py
@@ -3,3 +3,15 @@
 примера ссылки на файл можно использовать лицензию из ГитХаба из вашего репозитория:
 https://raw.githubusercontent.com/manti-by/lessons/master/LICENSE
 """
+
+from pathlib import Path
+import requests
+
+
+def load_file(link, name_in_system):
+    filename = Path(name_in_system)
+    url = link
+    response = requests.get(url)
+    filename.write_bytes(response.content)
+
+load_file('https://github.com/weibak/lessons/blob/master/LICENSE.txt', 'load_license_file.txt')
