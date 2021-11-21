5b7d5826b20854ec9ab4db90e70495603dc923ef
Artem Sheibak <sheibakaa@gmail.com>
2021-11-21 14:24:56 +0300

    add solution-02 file

--- lesson-05/solution-02.py
+++ lesson-05/solution-02.py
@@ -5,3 +5,30 @@
 выходе пары, кто и кому будет дарить, причем сам себе человек не может подарить, дубликаты
 тоже не допустимы.
 """
+
+import random
+
+
+my_list = input("Введите список имен через пробел: ").split()
+print("Ввденый список: ", my_list)
+
+
+random.shuffle(my_list)
+offset = [my_list[-1]] + my_list[:-1]
+for santa, receiver in zip(my_list, offset):
+     print(santa, "дарит подарок", receiver)
+
+"""другой вариант решения который я так и не смог допилить
+random.shuffle(new_my_list_1)
+new_my_list = my_list + new_my_list_1
+
+for i in range(len(new_my_list)):
+    if i % 2 == 0:
+        list1 = new_my_list
+        list2 = new_my_list[i-1]
+        for n in range(len(list1)):
+            new_my_list.append([list1[n], list2[n]])
+
+print(new_my_list)
+
+"""
