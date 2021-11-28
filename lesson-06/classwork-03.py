f60b03003e3ec7e4b92d0462bc884dee2edc717b
Artem Sheibak <sheibakaa@gmail.com>
2021-11-28 18:18:17 +0300

    ADD classwork-03 and solution-01 -02 files

--- lesson-06/classwork-03.py
+++ lesson-06/classwork-03.py
@@ -1,6 +1,14 @@
-'''
+"""
 Написать функцию которая возвращают случайным образом одну карту из стандартной колоды в 36 карт,
 где на первом месте номинал карты номинал (6 - 10, J, D, K, A), а на втором название масти
 (Hearts, Diamonds, Clubs, Spades).
+"""
 
-'''
+
+import random
+
+denomination = ['6', '7', '8', '9', '10', 'J', 'D', 'K', 'A']
+suit = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+
+my_string = f"Your card is: , {random.choice(denomination)} {random.choice(suit)}"
+print(my_string)
---
+++ lesson-06/solution-01.py
@@ -0,0 +1,28 @@
+"""
+Напишите программу, которая принимает текст и выводит два слова: наиболее часто встречающееся и самое длинное.
+"""
+
+
+word_list = input("Введите слова через пробел: ").split()
+print("Введеный список слов: ", word_list)
+
+
+new_word_list = {i: word_list.count(i) for i in word_list}
+maxcnt = 0
+theltr = 0
+for ltr in new_word_list:
+    ltrfoud = ltr
+    foundcont = new_word_list[ltr]
+    if foundcont > maxcnt:
+        maxcnt = foundcont
+        theltr = ltrfoud
+print('Наиболее часто повторяющееся: '  + theltr)
+
+
+"""maxlong = max(word_list)
+print('Самое длинное слово', maxlong)
+"""
+longword = []
+for i in word_list:
+        longword.append(len(i))
+print('Самое длинное слово: ', word_list[longword.index(max(longword))])
