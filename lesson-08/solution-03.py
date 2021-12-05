1f8cdda68a73b00741dc063a916b693f97912163
Artem Sheibak <sheibakaa@gmail.com>
2021-12-05 19:56:49 +0300

    ADD homework

--- lesson-08/solution-03.py
+++ lesson-08/solution-03.py
@@ -8,3 +8,30 @@
 Необходимо написать программу, которая для данного числа дисков n печатает
 последовательность перекладываний, необходимую для решения головоломки.
 """
+
+#один из вариантов решения, но как по мне работает кривовато
+"""
+def hanoi (n, first = 1, last = 3):
+    if n == 1:
+        print(f"Переложит диск {n} со стержня {first} на стержень {last}")
+    else:
+        hanoi(n - 1, first, 6 - first - last)
+        print(f"Переложит диск {n} со стержня {first} на стержень {last}")
+        hanoi(n - 1, 6 - first - last, last)
+
+
+count = int(input("Введите количество дисков: "))
+hanoi(count)
+"""
+
+def hanoi(height, fromePole, toPole, withPole):
+    if height >= 1:
+        hanoi(height -1, fromePole, withPole, toPole)
+        move_disk(fromePole, toPole)
+        hanoi(height - 1, withPole, toPole, fromePole)
+
+def move_disk(fp, tp):
+    print("Перемещение диска с ",fp,"на ",tp)
+
+n = int(input("Введите количество дисков: "))
+hanoi(n, "A", "B", "C")
