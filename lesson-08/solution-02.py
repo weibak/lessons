5ef268cc96a7da5df8529d68779ff421f53b0197
Artem Sheibak <sheibakaa@gmail.com>
2021-12-06 19:14:01 +0300

    ADD homework

--- lesson-08/solution-02.py
+++ lesson-08/solution-02.py
@@ -24,3 +24,59 @@ max_reply = max(max_reply, curr_reply)
 if __name__ == "__main__":
     print(previosly)
     print(max_reply)
+
+"""
+Дана последовательность натуральных чисел, завершающаяся числом 0. Определите, какое наибольшее число подряд идущих
+элементов этой последовательности равны друг другу и что это за элемент. Т.е. если программе на вход подать
+последовательность [1, 2, 2, 3, 7, 4, 4, 4, 0, 5, 5, 5], то на печать программа должна вывести числа 4 и 3,
+где 4 - повторяющийся элемент, 3 - количество повторений.
+"""
+
+
+def get_list_most_common_element(number_list: list) -> tuple:
+    result = {}
+
+    # Iterate over list and calculate max repetitions
+    previous_number = None
+    current_sequence = 0
+    for number in number_list:
+        if number == 0:
+            # Save last found result and break the loop
+            result[previous_number] = current_sequence
+            break
+
+        # Check if start new sequence
+        if previous_number == number:
+            current_sequence += 1
+        else:
+            # Check previous sequence and if current bigger - save it
+            # or simply set current value
+            if previous_number not in result:
+                result[previous_number] = current_sequence
+            elif result[previous_number] < current_sequence:
+                result[previous_number] = current_sequence
+
+            # restart current sequence
+            current_sequence = 1
+
+        # Remember number for the next iteration
+        previous_number = number
+
+    # Assume that the first element is max and try to find a bigger element
+    max_number = list(result.keys())[0]
+    max_repetitions = result[max_number]
+    for key, value in result.items():
+        if value > max_repetitions:
+            max_number = key
+            max_repetitions = value
+    return max_number, max_repetitions
+
+
+def main():
+    number_list = [1, 2, 2, 3, 7, 4, 4, 4, 0, 5, 5, 5]
+    max_number, max_repetitions = get_list_most_common_element(number_list)
+    print(max_number, max_repetitions)
+
+
+if __name__ == "__main__":
+    main()
--- lesson-08/solution-03.py
+++ lesson-08/solution-03.py
@@ -37,3 +37,94 @@ def move_disk(fp, tp):
 if __name__ == "__main__":
     n = int(input("Введите количество дисков: "))
     hanoi(n, "A", "B", "C")
+
+"""
+Головоломка “Ханойские башни” состоит из трех стержней, пронумерованных числами 1, 2, 3. На стержень 1 надета пирамидка
+из n дисков различного диаметра в порядке возрастания диаметра. Диски можно перекладывать с одного стержня на другой
+строго по одному, при этом диск нельзя класть на диск меньшего диаметра. Необходимо переложить всю пирамидку со
+стержня 1 на стержень 3 за минимальное число перекладываний.
+Необходимо написать программу, которая для данного числа дисков n печатает последовательность перекладываний,
+необходимую для решения головоломки.
+"""
+
+
+def hanoi_tower_loop(n):
+    # We assume that the first element on the top of tower
+    tower_01 = list(range(1, n + 1))
+    tower_02 = []
+    tower_03 = []
+
+    # We are moving elements while
+    previous_step_target = 0
+    while len(tower_01) or len(tower_02):
+        if tower_01 and previous_step_target != 1:
+            if not tower_02 or tower_02[0] > tower_01[0]:
+                print("Move disc", chr(64 + tower_01[0]), "From tower 1 to tower 2")
+                previous_step_target = 2
+
+                tower_02.insert(0, tower_01[0])
+                del tower_01[0]
+                continue
+
+            if not tower_03 or tower_03[0] > tower_01[0]:
+                print("Move disc", chr(64 + tower_01[0]), "From tower 1 to tower 3")
+                previous_step_target = 3
+
+                tower_03.insert(0, tower_01[0])
+                del tower_01[0]
+                continue
+
+        if tower_02 and previous_step_target != 2:
+            if not tower_03 or tower_03[0] > tower_02[0]:
+                print("Move disc", chr(64 + tower_02[0]), "From tower 2 to tower 3")
+                previous_step_target = 3
+
+                tower_03.insert(0, tower_02[0])
+                del tower_02[0]
+                continue
+
+            if not tower_01 or tower_01[0] > tower_02[0]:
+                print("Move disc", chr(64 + tower_02[0]), "From tower 2 to tower 1")
+                previous_step_target = 1
+
+                tower_01.insert(0, tower_02[0])
+                del tower_02[0]
+                continue
+
+        if tower_03 and previous_step_target != 3:
+            if not tower_02 or tower_02[0] > tower_03[0]:
+                print("Move disc", chr(64 + tower_03[0]), "From tower 3 to tower 2")
+                previous_step_target = 2
+
+                tower_02.insert(0, tower_03[0])
+                del tower_03[0]
+                continue
+
+            if not tower_01 or tower_01[0] > tower_03[0]:
+                print("Move disc", chr(64 + tower_03[0]), "From tower 3 to tower 1")
+                previous_step_target = 1
+
+                tower_01.insert(0, tower_03[0])
+                del tower_03[0]
+                continue
+
+
+def hanoi_tower_recursive(n, source, target, buffer):
+    if n > 0:
+        hanoi_tower_recursive(n - 1, source, buffer, target)
+        print("Move disc", chr(64 + n), "From tower", source + 1, "to tower", target + 1)
+        hanoi_tower_recursive(n - 1, buffer, target, source)
+
+
+def main():
+    print("Run Hanoi Tower in a loop")
+    hanoi_tower_loop(4)
+
+    print("\n ------------------------- \n")
+
+    print("Run Hanoi Tower recursively")
+    hanoi_tower_recursive(4, 0, 2, 1)
+
+
+if __name__ == "__main__":
+    main()
